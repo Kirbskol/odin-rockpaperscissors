03162ed61d6bc525fdd224cf15bc0dd5a4f60923
Kirbskol <138513974+Kirbskol@users.noreply.github.com>
2024-10-16 18:31:22 +0100

    Create playRound function and increment score depending on winner
    playRound function has been developed, whereby it uses getComputerChoice
    function and getHumanChoice function, and then decides winner depending
    on hand selections. Global score tracker is updated depending on result.

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function getComputerChoice() {
     else if (compNum > 80) {
         compChoice = "SCISSORS";
     }
+    return compChoice;
 }
   // obtains user input for humanChoice, re-prompts if not valid input
 function getHumanChoice() {
@@ -23,4 +24,23 @@ function getHumanChoice() {
     let userInput = prompt("Choose your hand: ");
     humanChoice = userInput.toUpperCase();
     } while ((humanChoice !== "ROCK") && (humanChoice !== "PAPER") && (humanChoice !== "SCISSORS"));
+    return humanChoice;
+}
+
+// play a single round of rock, paper, scissors.
+function playRound() {
+    const humanSelection = getHumanChoice();
+    const computerSelection = getComputerChoice();
+    console.log(`Player selects: ${humanSelection}\nComputer selects: ${computerSelection}`);
+    if ((humanSelection == "ROCK" && computerSelection == "ROCK") || (humanSelection == "PAPER" && computerSelection == "PAPER") || (humanSelection == "SCISSORS" && computerSelection == "SCISSORS")) {
+        console.log(`TIE round. ${humanSelection} and ${humanSelection} don't beat each other!`);
+    }
+    else if ((humanSelection == "ROCK" && computerSelection == "SCISSORS") || (humanSelection == "PAPER" && computerSelection == "ROCK") || (humanSelection == "SCISSORS" && computerSelection == "PAPER")) {
+        console.log(`PLAYER wins. ${humanSelection} beats ${computerSelection}!`);
+        humanScore++;
+    }
+    else if ((computerSelection == "ROCK" && humanSelection == "SCISSORS") || (computerSelection == "PAPER" && humanSelection == "ROCK") || (computerSelection == "SCISSORS" && humanSelection == "PAPER")) {
+        console.log(`COMPUTER wins. ${computerSelection} beats ${humanSelection}!`);
+        computerScore++;
+    }
 }
